fa6937e1fe0261aa830723b2ddf61008fba565a5
Sebastian Graf <sebastian.graf@kit.edu>
2024-01-19 11:41:11 +0100

    Fix build

---
+++ paper/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Build the document";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        texlive = pkgs.texlive.combined.scheme-full;
+      in {
+        packages.comp-trace = pkgs.stdenvNoCC.mkDerivation {
+          name = "comp-trace";
+          buildInputs = with pkgs; [
+            gnumake
+            texlive
+          ];
+          builder = "${pkgs.bash}/bin/bash";
+          args = [ "-c" ''
+            source $stdenv/setup
+            mkdir -p $out
+            cp -r $src/* .
+            make comp-trace.pdf comp-trace-ext.pdf
+            cp *.pdf $out/
+          ''];
+          src = ./.;
+          system = "x86_64-linux";
+        };
+
+        defaultPackage = self.packages.${system}.comp-trace;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
